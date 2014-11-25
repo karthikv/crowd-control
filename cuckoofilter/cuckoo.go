9c46086623b17bb78c1850ab932e42e9ba9e4161
gshubham <shubhamgoel123@gmail.com>
2014-11-25 10:52:33 -0800

    Fixed referencing bug in displace(). Switched to 2 bytes/fingerprint.

--- cuckoofilter/cuckoo.go
+++ cuckoofilter/cuckoo.go
@@ -1,14 +1,10 @@
 package cuckoofilter
 import (
-  // "bytes"
   "errors"
-  // "strings"
-  // "fmt"
   "crypto/sha256"
   "math/rand"
-  "fmt"
+  // "fmt"
   "math/big"
-  // "time"
 )
 // Constants
 const (
@@ -22,7 +18,7 @@ type CuckooFilter struct {
 }
 // Represents a bucket in each position of the hash table
 type bucketList struct {
-  fingerprints [BUCKET_SIZE]byte // 4 fingerprints
+  fingerprints [BUCKET_SIZE]uint16 // 4 fingerprints
   numElements uint8 // number of elements
 }
 
@@ -44,7 +40,6 @@ func choose(firstIndex, secondIndex uint64) uint64 {
 
 // Given two byte arrays, returns a new XORed byte array 
 func xor(a, b []byte) []byte {
-  // fmt.Println("Length of XOR arrays are: ", len(a), " and " ,len(b))
   var min int
   if len(a) < len(b) {
     min = len(a)
@@ -58,11 +53,12 @@ func xor(a, b []byte) []byte {
   return result
 }
 
-func xorAllBytes(a []byte) byte {
-  var result byte
-  result = a[0]
-  for i := 0; i < len(a); i++ {
-    result = result ^ a[i]
+func xorAllBytes(a []byte) uint16 {
+  var result uint16
+  result = uint16(a[0]) << 8
+  result = result | uint16(a[1])
+  for i := 2; i < len(a); i++ {
+    result = result ^ uint16(a[i])
   }
   return result
 }
@@ -71,7 +67,6 @@ func Make(size int) (*CuckooFilter, error) {
   if size <= 0 {
     return nil, errors.New("Specify a positive size.")
   }
-  // fmt.Println("Size is", size)
   cf := &CuckooFilter { 
     size: size, 
     hashtable: make([]bucketList, size)}
@@ -81,27 +76,25 @@ func Make(size int) (*CuckooFilter, error) {
 /**** Private methods *****/
 
 // Takes in a fingerprint and a bucket index, and inserts the fingerprint at the end if possible. Otherwise, returns false. Doesn't displace anything.
-func (cf *CuckooFilter) insert(index uint64, fingerprint byte) bool {
+func (cf *CuckooFilter) insert(index uint64, fingerprint uint16) bool {
   bucket := &cf.hashtable[index]
   if bucket.numElements < BUCKET_SIZE {
     bucket.fingerprints[bucket.numElements] = fingerprint
     bucket.numElements++
-    // fmt.Println("Number of elements in the bucket is:", bucket.numElements)
     return true
   }
   return false
 }
 
 // This function is responsible for the chained displacements of fingerprints. Forces an insert of fingerprint and the relevant index, and then chains displace for the evicted fingerprint. Returns false ('too full') in case number of displacements is over MAX_DISPLACEMENTS
-func (cf *CuckooFilter) displace(fingerprint byte, index uint64) bool {
-  fmt.Println("In displace...")
+func (cf *CuckooFilter) displace(fingerprint uint16, index uint64) bool {
   var displacements int = 1
   firstIndex, intermediateXor, newIndex, cuckooSize := big.NewInt(int64(index)), new(big.Int), new(big.Int), big.NewInt(int64(cf.size))
   for displacements < MAX_DISPLACEMENTS {
     if cf.insert(index, fingerprint) {
       return true
     }
-    currBucket := cf.hashtable[index]
+    currBucket := &cf.hashtable[index]
     displaceIndex := rand.Intn(BUCKET_SIZE)
 
     // Swap out the old fingerprint
@@ -110,7 +103,7 @@ func (cf *CuckooFilter) displace(fingerprint byte, index uint64) bool {
 
     // Calculate secondary index of the old fingerprint
     _ = firstIndex.SetInt64(int64(index))
-    intermediateXor.SetBytes(xor(firstIndex.Bytes(), hash(string(fingerprint))))
+    intermediateXor.SetBytes(xor(firstIndex.Bytes(), hash(string(oldFingerprint))))
     newIndex.Mod(intermediateXor, cuckooSize)
     // Apply displace on the old fingerprint
     index = newIndex.Uint64()
@@ -121,13 +114,12 @@ func (cf *CuckooFilter) displace(fingerprint byte, index uint64) bool {
 }
 
 // This function calculates the relevant first and second bucket indices of the fingerprint.
-func (cf *CuckooFilter) calculateIndices(checksum []byte, fingerprint byte) (uint64, uint64) {
+func (cf *CuckooFilter) calculateIndices(checksum []byte, fingerprint uint16) (uint64, uint64) {
   checksumNumber, firstIndex, secondIndex, intermediateXor := new(big.Int), new(big.Int), new(big.Int), new(big.Int)
   cuckooSize := big.NewInt(int64(cf.size))
 
   _ = checksumNumber.SetBytes(checksum)
   _ = firstIndex.Mod(checksumNumber, cuckooSize)
-  // fmt.Println(firstIndex.Bytes())
   intermediateXor.SetBytes(xor(firstIndex.Bytes(), hash(string(fingerprint))))
   secondIndex.Mod(intermediateXor, cuckooSize)
 
@@ -139,7 +131,6 @@ func (cf *CuckooFilter) contains(key string) (bool, uint64, int) {
   checksum := hash(key)
   fingerprint := xorAllBytes(checksum)
   firstIndex, secondIndex := cf.calculateIndices(checksum, fingerprint)
-  // fmt.Println("In contains. First and second indices are: ", firstIndex, secondIndex)
   fingerprintIndex, present := cf.search(firstIndex, fingerprint)
   if present {
     return true, firstIndex, fingerprintIndex
@@ -153,9 +144,8 @@ func (cf *CuckooFilter) contains(key string) (bool, uint64, int) {
 }
 
 // Search bucket for fingerprint. Returns (index, present). If not found, present is false
-func (cf *CuckooFilter) search(index uint64, fingerprint byte) (int, bool){
+func (cf *CuckooFilter) search(index uint64, fingerprint uint16) (int, bool){
   currBucket := &cf.hashtable[index]
-  // fmt.Println("In search. Number of elements is... ", currBucket.numElements)
   i := uint8(0)
   for i = 0; i < currBucket.numElements; i++ {
     if currBucket.fingerprints[i] == fingerprint {
@@ -178,7 +168,6 @@ func (cf *CuckooFilter) Add(key string) bool {
   // TODO: Initialize random generator
   // 8-byte SHA checksum of the key
   if cf.Contains(key) {
-    fmt.Println("Returning...")
     return false
   }
   checksum := hash(key)
@@ -186,11 +175,9 @@ func (cf *CuckooFilter) Add(key string) bool {
   fingerprint := xorAllBytes(checksum)
   // find indices of the fingerprint in the filter
   firstIndex, secondIndex := cf.calculateIndices(checksum, fingerprint)
-  // fmt.Println("In add. First index and second index are ", firstIndex, secondIndex)
   // Try to insert at first index
   tryFirst := cf.insert(firstIndex, fingerprint)
   if !tryFirst {
-    // fmt.Println("I'm here..")
     // try at second
     trySecond := cf.insert(secondIndex, fingerprint)
     if !trySecond {
@@ -201,7 +188,6 @@ func (cf *CuckooFilter) Add(key string) bool {
     }
     return true
   }
-  // fmt.Println("Finishing add. Number of elements is now: ", cf.hashtable[0].numElements)
   return true
 }
 
--- cuckoofilter/test_test.go
+++ cuckoofilter/test_test.go
@@ -17,16 +17,16 @@ func TestBasic(*testing.T) {
 func TestIntermediate(*testing.T) {
   cf, _ := Make(64)
   var result bool
-  for i := 0; i < 200; i++ {
-    s := "test" + strconv.Itoa(i)
+  for i := 0; i < 250; i++ {
+    s := strconv.Itoa(i)
     result = cf.Add(s)
     if !result {
       fmt.Println("Add failed. This isn't good.", s)
       break
     }
   }
-  for j := 0; j < 200; j++ {
-    s := "test" + strconv.Itoa(j)
+  for j := 0; j < 250; j++ {
+    s := strconv.Itoa(j)
     result = cf.Contains(s)
     if !result {
       fmt.Println("Contains failed. This is bad. ", s)
