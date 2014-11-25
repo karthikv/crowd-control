02599e958d897d4e78cec4a9dc0ff22f8f0cfeec
gshubham <shubhamgoel123@gmail.com>
2014-11-24 19:49:21 -0800

    Indices are now calculated the correct way

--- test_test.go
+++ test_test.go
@@ -4,15 +4,35 @@ import (
   "fmt"
   "strconv"
 )
-func TestCuckooFilter(*testing.T) {
+func TestBasic(*testing.T) {
   cf, _ := Make(8)
-  for i := 0; i < 1000; i++ {
-    s := "test" + strconv.Itoa(i)
-    _ = cf.Add(s)
-  }
+  _ = cf.Add("test")
   result := cf.Contains("test")
   fmt.Println(result)
   cf.Delete("test")
   result = cf.Contains("test")
   fmt.Println(result)
+}
+
+func TestIntermediate(*testing.T) {
+  cf, _ := Make(1000)
+  var result bool
+  for i := 0; i < 700; i++ {
+    s := "test" + strconv.Itoa(i)
+    result = cf.Add(s)
+    if !result {
+      fmt.Println("Add failed. This isn't good.")
+    }
+  }
+  for j := 0; j < 700; i++ {
+    s := "test" + strconv.Itoa(i)
+    result = cf.Contains("test" + strconv.Itoa(i))
+    if !result {
+      fmt.Println("Contains failed. This is bad.")
+    }
+  }
+}
+
+func TestAdvanced(*testing.T) {
+
 }
